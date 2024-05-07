63dcf253ca4ea45dba711c6ed0a85649a33d5a19
RDKone <rdonasik@gmail.com>
2024-05-07 12:58:19 -0400

    removed @WebServlet

--- src/main/java/com/example/sfaaproj/HomeServlet.java
+++ src/main/java/com/example/sfaaproj/HomeServlet.java
@@ -7,7 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "homeServlet", value = "/home")
 public class HomeServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
